dd0ee62390b38715bb651fcfa046e5c94a2fed21
Lucas <carneirolucas88@gmail.com>
2023-10-14 01:00:37 -0300

    Cria ErrorPage.jsx

---
+++ project_frontend/src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,20 @@
+// import { useNavigate } from "react-router-dom";
+
+function ErrorPage() {
+    // const navigate = useNavigate()
+    return (
+        <>
+           
+            <h1>Error 404</h1>
+            <p> Ops! <br />
+                Não conseguimos encontrar a página <br /> que você esta procurando.
+                <br /></p>
+            {/* <Button type="submit" onClick={() => {
+                navigate("/");
+            }}
+                variant="primary" id="btnLogin">Voltar para o início</Button> */}
+        </>
+    );
+}
+
+export default ErrorPage;
