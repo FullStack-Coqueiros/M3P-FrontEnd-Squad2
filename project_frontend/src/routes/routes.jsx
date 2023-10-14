f29e33c60e4499d5744f321d06d17d8a2df7884c
Lucas <carneirolucas88@gmail.com>
2023-10-14 01:01:15 -0300

    ErrorPage configurada no routes

--- project_frontend/src/routes/routes.jsx
+++ project_frontend/src/routes/routes.jsx
@@ -1,12 +1,14 @@
 // routes.jsx
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CadastroExames from "../pages/CadastroExames/CadastroExames";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/cadastro-exames" element={<CadastroExames />} />
+        <Route path="/error-page" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
