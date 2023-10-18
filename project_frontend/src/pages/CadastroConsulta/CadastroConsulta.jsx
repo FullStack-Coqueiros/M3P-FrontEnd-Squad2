7d2a8fc9c48bd298a962ae6f271b2078358e5fc5
Lucas <carneirolucas88@gmail.com>
2023-10-18 04:24:50 -0300

    cria page cadastro consulta

---
+++ project_frontend/src/pages/CadastroConsulta/CadastroConsulta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CadastroConsulta() {
+  return (
+    <div>
+    <h1>  Olá eu sou o cadastro consulta</h1>
+    </div>
+  )
+}
+
+export default CadastroConsulta
