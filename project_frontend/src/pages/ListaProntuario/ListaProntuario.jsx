98454601c5e23b059010a4a7b81e39b1f479376c
Lucas <carneirolucas88@gmail.com>
2023-10-18 04:19:52 -0300

    cria lista prontuario

---
+++ project_frontend/src/pages/ListaProntuario/ListaProntuario.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListaProntuario() {
+  return (
+    <div>
+      <h1>Olá eu sou a lista de prontuário</h1>
+    </div>
+  )
+}
+
+export default ListaProntuario
