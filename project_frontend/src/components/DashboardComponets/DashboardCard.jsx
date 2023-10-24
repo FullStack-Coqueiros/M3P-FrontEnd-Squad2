2a86ae089f5c28534190f08db61f7a96a73f60e3
Lucas <carneirolucas88@gmail.com>
2023-10-24 15:32:00 -0300

    ajustes de renderização no Dashboard.

--- project_frontend/src/components/DashboardComponets/DashboardCard.jsx
+++ project_frontend/src/components/DashboardComponets/DashboardCard.jsx
@@ -1,16 +1,55 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
 
-function DashboardCard({ titulo, quantidade, color }) {
+
+
+const DashboardCard = ({ estatisticas }) => {
+  const chartRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    const newCtx = canvasRef.current.getContext("2d");
+
+    chartRef.current = new Chart(newCtx, {
+      type: "bar",
+      data: {
+        labels: estatisticas.map((item) => item.titulo),
+        datasets: [
+          {
+            label: "Gráfico de estatísticas",
+            backgroundColor: estatisticas.map((item) => item.color || "rgba(192, 171, 75, 0.2)"),
+            borderColor: ["black"], 
+            borderWidth: 1.0,
+            data: estatisticas.map((item) => item.quantidade),
+          },
+          
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            type: 'linear',
+            beginAtZero: true,
+            precision: 0,
+          },
+        },
+        animation: {
+          duration: 1000,
+          easing: 'easeInOutQuad',
+        },
+      },
+    });
+  }, [estatisticas]);
+
   return (
-    <Card className={`dashboard-card ${color ? `card-${color}` : ""}`}>
-      <Card.Body>
-        <Card.Title>{titulo}</Card.Title>
-        <Card.Text>{quantidade}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div className="dashboard-card">
+      <canvas ref={canvasRef} id={`${estatisticas[0].titulo}-chart`}></canvas>
+    </div>
   );
-}
+};
 
 export default DashboardCard;
-
--- project_frontend/src/pages/Dashboard/Dashboard.jsx
+++ project_frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,80 +1,206 @@
 import React, { useState, useEffect } from "react";
-import { Card, Col, Button, Container, Row } from "react-bootstrap";
+import { Card, Col, Button, Container, Row, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import {IoPersonCircleOutline } from 'react-icons/io5';
+import { IoPersonCircleOutline } from 'react-icons/io5';
 
-import DashboardCard from "../../components/DashboardComponets/DashboardCard";
 import Sidebar from "../../components/SidebarComponents/Sidebar";
+import DashboardCard from "../../components/DashboardComponets/DashboardCard";
 
 import { URL_API } from "../../services";
 
+
 function Dashboard() {
+  const [usuarios, setUsuarios] = useState([]);
   const [pacientes, setPacientes] = useState([]);
   const [consultas, setConsultas] = useState([]);
   const [exames, setExames] = useState([]);
+  const [medicamento, setMedicamento] = useState([]);
+  const [dietas, setDietas] = useState([]);
+  const [exercicios, setExercicios] = useState([]);
+  const [termoPesquisa, setTermoPesquisa] = useState("");
+  const [termoPesquisaUsuario, setTermoPesquisaUsuario] = useState("");
+
+  const isAdmin = true;// Esta parte esta para teste 
+  // const isAdmin = false; // Esta parte esta para teste 
 
   useEffect(() => {
+    fetch(`${URL_API}/usuarios`)
+      .then(response => response.json())
+      .then(data => setUsuarios(data))
+      .catch(error => console.error('Erro ao buscar dados:', error));
+
     fetch(`${URL_API}/pacientes`)
       .then(response => response.json())
       .then(data => setPacientes(data))
       .catch(error => console.error('Erro ao buscar dados:', error));
 
-      fetch(`${URL_API}/consultas`)
+    fetch(`${URL_API}/consultas`)
       .then(response => response.json())
       .then(data => setConsultas(data))
       .catch(error => console.error('Erro ao buscar dados:', error));
 
-      fetch(`${URL_API}/exames`)
+    fetch(`${URL_API}/exames`)
       .then(response => response.json())
       .then(data => setExames(data))
       .catch(error => console.error('Erro ao buscar dados:', error));
 
+    fetch(`${URL_API}/medicamentos`)
+      .then(response => response.json())
+      .then(data => setMedicamento(data))
+      .catch(error => console.error('Erro ao buscar dados:', error));
 
-    // Fetch para consultas e exames aqui (semelhante ao exemplo acima)
+    fetch(`${URL_API}/dietas`)
+      .then(response => response.json())
+      .then(data => setDietas(data))
+      .catch(error => console.error('Erro ao buscar dados:', error));
+
+    fetch(`${URL_API}/exercicios`)
+      .then(response => response.json())
+      .then(data => setExercicios(data))
+      .catch(error => console.error('Erro ao buscar dados:', error));
   }, []);
 
   return (
     <>
       <Sidebar />
-
       <Container>
-        <h1>Painel de Controle</h1>
-        <Row>
-          <Col>
-            <DashboardCard titulo="Pacientes Cadastrados" quantidade={pacientes.length} />
-          </Col>
-          <Col>
-            <DashboardCard titulo="Consultas Realizadas" quantidade={consultas.length} />
-          </Col>
-          <Col>
-            <DashboardCard titulo="Exames Realizados" quantidade={exames.length} />
-          </Col>
-        </Row>
 
-        <h1>PACIENTES</h1>
-        <div className="cards">
-          <Row xs={1} sm={2} md={3} className="mb-4">
-            {pacientes.map((paciente) => (
-              <Col key={paciente.id} className="mb-4">
-                <Card style={{ width: "300px", height: "400px" }} className="text-center">
-                  <IoPersonCircleOutline className="foto mt-3" size={150} color="#000" /> 
-                  <Card.Body className="mt-3">
-                    <Card.Title>
-                      <div>{paciente.nomeCompleto}</div>
-                      <div>Nascimento: {paciente.dataNascimento}</div>
-                      <div>Telefone: {paciente.telefone}</div>
-                    </Card.Title>
-                  </Card.Body>
-                  <Col xs={12}>
-                    <Link to={`/prontuario${paciente.id}`}>
-                      <Button className="btn-ver">Ver mais</Button>
-                    </Link>
+        {isAdmin ? (
+          // Se for administrador
+          <>
+        <h1>Estatísticas do Sistema</h1>
+        <DashboardCard estatisticas={[
+  { titulo: "Usuários Cadastrados", quantidade: usuarios.length, color: "#192fac" },
+  { titulo: "Pacientes Cadastrados", quantidade: pacientes.length, color: "rgb(100, 161, 231)" },
+  { titulo: "Consultas Realizadas", quantidade: consultas.length, color: "#9bc6e2" },
+  { titulo: "Exames Realizados", quantidade: exames.length, color: "#88def3" },
+  { titulo: "Dietas Realizadas", quantidade: dietas.length, color: "#23dada" },
+  { titulo: "Medicamentos prescritos", quantidade: medicamento.length, color: "#1dd1aa" },
+  { titulo: "Exercícios solicitados", quantidade: exercicios.length, color: "#33ff66" },
+]} />
+            <h1>Pesquise o usuario</h1>
+            <div className="cards">
+              <div className="d-flex justify-content-center mb-4">
+                <Form.Control
+                  type="text"
+                  placeholder="Pesquisar por nome, CPF, telefone ou e-mail"
+                  value={termoPesquisaUsuario}
+                  onChange={(e) => setTermoPesquisaUsuario(e.target.value)}
+                />
+              </div>
+            </div>
+            <Row xs={1} sm={2} md={3} className="mb-4">
+              {usuarios
+                .filter((usuario) => {
+                  const termoLowerCase = termoPesquisaUsuario.toLowerCase();
+                  return (
+                    usuario.nome.toLowerCase().includes(termoLowerCase) ||
+                    usuario.cpf.includes(termoPesquisaUsuario) ||
+                    usuario.telefone.includes(termoPesquisaUsuario) ||
+                    usuario.email.toLowerCase().includes(termoLowerCase)
+                  );
+                })
+                  .map((usuario, index) => (
+                    <Col key={index} className="mb-4">
+                      <Card style={{ width: "300px", height: "400px" }} className="text-center d-flex flex-column align-items-center">
+                        <IoPersonCircleOutline className="foto mt-3" size={150} color="#000" />
+                        <Card.Body className="mt-3">
+                          <Card.Title>
+                            <div>{usuario.nome}</div>
+                            <div>{usuario.tipo}</div>
+                            <div>{usuario.genero}</div>
+                            {/* <div>{usuario.cpf}</div> */}
+                            <div>{usuario.telefone}</div>
+                            <div>{usuario.email}</div>
+                          </Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ))}
+            </Row>
+
+
+            <h1>Pesquise o paciente</h1>
+            <div className="cards">
+              <div className="d-flex justify-content-center mb-4">
+                <Form.Control
+                  type="text"
+                  placeholder="Pesquisar por nome, CPF, telefone ou e-mail"
+                  value={termoPesquisa}
+                  onChange={(e) => setTermoPesquisa(e.target.value)}
+                />
+              </div>
+            </div>
+            <Row xs={1} sm={2} md={3} className="mb-4">
+              {pacientes
+                .filter((paciente) => {
+                  const termoLowerCase = termoPesquisa.toLowerCase();
+                  return (
+                    paciente.nomeCompleto.toLowerCase().includes(termoLowerCase) ||
+                    paciente.cpf.includes(termoPesquisa) ||
+                    paciente.telefone.includes(termoPesquisa) ||
+                    paciente.email.toLowerCase().includes(termoLowerCase)
+                  );
+                })
+                .map((paciente) => (
+                  <Col key={paciente.id} className="mb-4">
+                    <Card style={{ width: "300px", height: "400px" }} className="text-center d-flex flex-column align-items-center">
+                      <IoPersonCircleOutline className="foto mt-3" size={150} color="#000" />
+                      <Card.Body className="mt-3">
+                        <Card.Title>
+                          <div>{paciente.nomeCompleto}</div>
+                          <div>Nascimento: {paciente.dataNascimento}</div>
+                          <div>Telefone: {paciente.telefone}</div>
+                          <div>Email: {paciente.email}</div>
+                        </Card.Title>
+                      </Card.Body>
+                      <Col xs={12}>
+                        <Link to={`/Prontuario${paciente.id}`}>
+                          <Button className="btn-ver">Ver mais</Button>
+                        </Link>
+                      </Col>
+                    </Card>
                   </Col>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
+                ))}
+            </Row>
+          </>
+        ) : (
+          // Se não for administrador
+          <>
+            <h2>PACIENTES</h2>
+            <Row xs={1} sm={2} md={3} className="mb-4">
+              {pacientes
+                .filter((paciente) => {
+                  const termoLowerCase = termoPesquisa.toLowerCase();
+                  return (
+                    paciente.nomeCompleto.toLowerCase().includes(termoLowerCase) ||
+                    paciente.cpf.includes(termoPesquisa) ||
+                    paciente.telefone.includes(termoPesquisa) ||
+                    paciente.email.toLowerCase().includes(termoLowerCase)
+                  );
+                })
+                .map((paciente) => (
+                  <Col key={paciente.id} className="mb-4">
+                    <Card style={{ width: "300px", height: "400px" }} className="text-center">
+                      <IoPersonCircleOutline className="foto mt-3" size={150} color="#000" />
+                      <Card.Body className="mt-3">
+                        <Card.Title>
+                          <div>{paciente.nomeCompleto}</div>
+                          <div>Nascimento: {paciente.dataNascimento}</div>
+                          <div>Telefone: {paciente.telefone}</div>
+                        </Card.Title>
+                      </Card.Body>
+                      <Col xs={12}>
+                        <Link to={`/Prontuario${paciente.id}`}>
+                          <Button className="btn-ver">Ver mais</Button>
+                        </Link>
+                      </Col>
+                    </Card>
+                  </Col>
+                ))}
+            </Row>
+          </>
+        )}
       </Container>
     </>
   );
