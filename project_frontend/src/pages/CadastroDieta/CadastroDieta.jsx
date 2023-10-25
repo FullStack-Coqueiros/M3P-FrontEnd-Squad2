35e66c952fa8ff940a69fff461f0c9efc64f05e8
Lucas <carneirolucas88@gmail.com>
2023-10-25 19:49:38 -0300

    refaturacao cadastro dieta

--- project_frontend/src/pages/CadastroDieta/CadastroDieta.jsx
+++ project_frontend/src/pages/CadastroDieta/CadastroDieta.jsx
@@ -1,125 +1,157 @@
-import React, { useState } from "react";
-import { Form, Button, Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useAppContext } from "../../context/useAppContext";
 import Sidebar from "../../components/SidebarComponents/Sidebar";
-import { Link } from "react-router-dom";
 
-// TODO refaturar codigo com base em cadastros como exames,consultas e medicamentos
 function CadastroDieta() {
-
-  const { handleAdicionarDieta, pacientes } = useAppContext();
-  const [isSaved, setIsSaved] = useState(false);
+  const { handleAdicionarDieta, pacientes, carregarPacientes } = useAppContext();
   const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  useEffect(() => {
+    carregarPacientes();
+  }, []);
 
-  function onSubmit(data) {
-    const dieta = {
-      ...data,
-    
+  function createDieta(dieta) {
+    const novaDieta = {
+      ...dieta,
+      pacienteId: Number(dieta.pacienteId),
     };
-
-    handleAdicionarDieta(dieta);
-    setIsSaved(true);
+    handleAdicionarDieta(novaDieta);
     setShowSuccessAlert(true);
+
+    // Limpar o formulário
+    reset();
+    // Imprimir os dados no console
+    console.log('Dados da Dieta para o paciente:', dieta);
   }
 
   return (
     <div>
-       <Sidebar />
+      <Sidebar />
       <Container>
-     
-
-
-        <h1>INFORME OS CAMPOS PARA CADASTRAR DIETA</h1>
         <section className="form-med">
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(createDieta)}>
+            <h2>Cadastro Dietas</h2>
+            <Row>
+              <Col>
+                <Form.Group controlId="pacienteId">
+                  <Form.Label>Selecione o paciente:</Form.Label>
+                  <Form.Select
+                    {...register("pacienteId", { required: true })}
+                  >
+                    {pacientes.map((paciente) => (
+                      <option key={paciente.id} value={paciente.id}>
+                        {paciente.nomeCompleto}
+                      </option>
+                    ))}
+                  </Form.Select>
+                  {errors.pacienteId && (
+                    <span className="error-message">Campo Obrigatório</span>
+                  )}
+                </Form.Group>
 
-            <div className="form-section">
-              <Form.Group controlId="pacienteId">
-                <Form.Label>Selecione o paciente:</Form.Label>
-                <Form.Select
-                  {...register("pacienteId", { required: true })}
-                >
-                  <h3>Selecione o paciente</h3>
-                  {pacientes.map(paciente => (
-                    <option key={paciente.id} value={paciente.id}>
-                      {paciente.nomeCompleto}
-                    </option>
-                  ))}
-                </Form.Select>
-                {errors.pacienteId && (<span className="error-message">Campo Obrigatório</span>)}
-              </Form.Group>
-
-
-              <Form.Group name="dieta">
-                <Form.Label>Nome da dieta:</Form.Label>
-                <Form.Control type="text" placeholder="Nome da dieta" {...register("dieta", { required: true, minLength: 5, maxLength: 100 })} />
-                {errors.dieta && <span className="error-message">Campo Obrigatório</span>}
-              </Form.Group>
-
-              <Form.Group name="data">
-                <Form.Label>Data:</Form.Label>
-                <Form.Control type="date" {...register("data", { required: true })} />
-                {errors.data && <span className="error-message">Campo Obrigatório</span>}
-              </Form.Group>
-
-              <Form.Group name="horario">
-                <Form.Label>Horário:</Form.Label>
-                <Form.Control type="time" defaultValue={new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-                  {...register("horario", { required: true })} />
-                {errors.horario && <span className="error-message">Campo Obrigatório</span>}
-              </Form.Group>
-
-              <Form.Group name="tipo">
-                <Form.Label>Tipo:</Form.Label>
-                <Form.Select {...register("tipo", { required: true })} >
-                  <option>Selecione</option>
-                  <option>Low Carb</option>
-                  <option>Dash</option>
-                  <option>Paleolítica</option>
-                  <option>Cetogênica</option>
-                  <option>Dukan</option>
-                  <option>Mediterrânea</option>
-                  <option>Outra</option>
-                </Form.Select>
-                {errors.tipo && <span className="error-message">Campo Obrigatório</span>}
-              </Form.Group>
-
-              <Form.Group name="descricao">
-                <Form.Label>Descrição:</Form.Label>
-                <Form.Control as="textarea" rows={5} placeholder="Descrição" {...register("descricao", { required: true, minLength: 10, maxLength: 1000 })} />
-                {errors.descricao && <span className="error-message">Campo Obrigatório</span>}
-              </Form.Group>
-
+              </Col>
+              <Col>
+                <Form.Group controlId="dieta">
+                  <Form.Label>Nome da dieta:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Nome da dieta"
+                    {...register("dieta", {
+                      required: true,
+                      minLength: 5,
+                      maxLength: 100,
+                    })}
+                  />
+                  {errors.dieta && (
+                    <span className="error-message">Campo Obrigatório  de 5 a 100 caracteres</span>
+                  )}
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group name="horario">
+                  <Form.Label>Horário:</Form.Label>
+                  <Form.Control type="time" defaultValue={new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                    {...register("horario", { required: true })} />
+                  {errors.horario && <span className="error-message">Campo Obrigatório</span>}
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group name="data">
+                  <Form.Label>Data:</Form.Label>
+                  <Form.Control type="date" {...register("data", { required: true })} />
+                  {errors.data && <span className="error-message">Campo Obrigatório</span>}
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group name="tipo">
+                  <Form.Label>Tipo:</Form.Label>
+                  <Form.Select {...register("tipo", { required: true })} >
+                    <option>Selecione</option>
+                    <option>Low Carb</option>
+                    <option>Dash</option>
+                    <option>Paleolítica</option>
+                    <option>Cetogênica</option>
+                    <option>Dukan</option>
+                    <option>Mediterrânea</option>
+                    <option>Outra</option>
+                  </Form.Select>
+                  {errors.tipo && <span className="error-message">Campo Obrigatório</span>}
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group name="descricao">
+                  <Form.Label>Descrição:</Form.Label>
+                  <Form.Control as="textarea" rows={5} placeholder="Descrição" {...register("descricao", { required: true, minLength: 10, maxLength: 1000 })} />
+                  {errors.descricao && <span className="error-message">Campo Obrigatório  de 10 a 1000 caracteres</span>}
+                </Form.Group>
+              </Col>
+            </Row>
+            <Col>
               <Form.Group name="statusSistema">
-                <Form.Label>Status do sistema:</Form.Label>
-                <Form.Control type="isValid" placeholder="Status do sistema" {...register("statusSistema", { required: true })} />
-                {errors.statusSistema && <span className="error-message">Campo Obrigatório</span>}
+                <Form.Label>Status do Sistema:</Form.Label>
+                <div>
+                  <Form.Check
+                    type="radio"
+                    label="Ativo"
+                    value="Ativo"
+                    {...register("statusSistema", { required: "Selecione o status do sistema." })}
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Inativo"
+                    value="Inativo"
+                    {...register("statusSistema", { required: "Selecione o status do sistema." })}
+                  />
+                </div>
+                {errors.statusSistema && (
+                  <span className="error-message">{errors.statusSistema.message}</span>
+                )}
               </Form.Group>
+            </Col>
 
+            <div>
+              <Button className="btn-salvar" type="submit">
+                Salvar
+              </Button>
             </div>
-
-            <div >
-              <Button className="btn-salvar" type="submit" > Salvar </Button>
-              <Button className="btn-editar" type="button" disable={!handleAdicionarDieta} > Editar </Button>
-              <Button className="btn-reset" type="reset" disable={!handleAdicionarDieta} > Deletar </Button>
-            </div>
-
           </form>
 
-          {showSuccessAlert && (<div className="alert alert-success mt-3"> Dieta cadastrada com sucesso! </div> )}
-
+          {showSuccessAlert && (
+            <div className="alert alert-success mt-3">
+              Dieta cadastrada com sucesso!
+            </div>
+          )}
         </section>
       </Container>
-     
     </div>
-  )
+  );
 }
 
-export default CadastroDieta
+export default CadastroDieta;
