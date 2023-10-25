a9564f195f227d8a7e703e382d2f35f20600a948
Lucas <carneirolucas88@gmail.com>
2023-10-25 14:53:55 -0300

    adiciona funcionalidades ao componente

--- project_frontend/src/components/DashboardComponets/DashboardCard.jsx
+++ project_frontend/src/components/DashboardComponets/DashboardCard.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
 
-
-
 const DashboardCard = ({ estatisticas }) => {
   const chartRef = useRef(null);
   const canvasRef = useRef(null);
@@ -22,11 +20,10 @@ const DashboardCard = ({ estatisticas }) => {
           {
             label: "Gráfico de estatísticas",
             backgroundColor: estatisticas.map((item) => item.color || "rgba(192, 171, 75, 0.2)"),
-            borderColor: ["black"], 
+            borderColor: ["black"],
             borderWidth: 1.0,
             data: estatisticas.map((item) => item.quantidade),
           },
-          
         ],
       },
       options: {
@@ -36,7 +33,7 @@ const DashboardCard = ({ estatisticas }) => {
             beginAtZero: true,
             precision: 0,
             ticks: {
-              stepSize: 1, // Define o intervalo entre os ticks como 1 para mostrar apenas números inteiros
+              stepSize: 1,
             },
           },
         },
@@ -45,9 +42,6 @@ const DashboardCard = ({ estatisticas }) => {
           easing: 'easeInOutQuad',
         },
       },
-      
-      
-      
     });
   }, [estatisticas]);
 
