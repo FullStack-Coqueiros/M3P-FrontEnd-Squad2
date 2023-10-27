fcb49e05a88c4441959c4b5674dedac4f596a73c
josicaroline <joseanecaroline@hotmail.com>
2023-10-26 21:35:39 -0300

    att login page

--- project_frontend/src/pages/Conta/criarConta.jsx
+++ project_frontend/src/pages/Conta/criarConta.jsx
@@ -1,22 +1,23 @@
 import { Button, Navbar } from 'react-bootstrap';
-
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+
 
 
 function CriarConta() {
     const navigate = useNavigate()
     return (
-        <>
+        <div>
             < Navbar />
-            <h1>Nóss desculpe!</h1>
-            <p> Infelizmente não será possível completar sua solicitação!
-                <br>Por favor, entre em contato com um administrador para novo cadastro!</br></p>
+            <h2>Nós desculpe!</h2>
+            <h4> Infelizmente não será possível completar sua solicitação! </h4>
+                <p>Por favor, entre em contato com um administrador para novo cadastro!</p>
             <Button type="submit" onClick={() => {
-                navigate("/login");
+                navigate("/");
             }}
                 variant="primary" id="btnLogin">Voltar para o início</Button>
-        </>
+        </div>
     );
 }
 
-export default CriarConta;
+export default CriarConta
--- project_frontend/src/pages/Reset/RecSenha.jsx
+++ project_frontend/src/pages/Reset/RecSenha.jsx
@@ -4,8 +4,6 @@ import { object, string } from 'yup';
 import { Form, Button, Navbar, } from 'react-bootstrap';
 
 
-//precisa resetar a senha
-
 const schema = object({
     email: string().required("Campo Obrigatório."),
     password: string().required("Campo Obrigatório.").min(6, "A senha precisa de 6 ou mais caracteres."),
@@ -20,16 +18,15 @@ function RecuperarSenha() {
     return (
         <>
             <Navbar />
-                <h1>Esqueceu sua senha?</h1>
                 <section className="area-login">
                 <form className="form-login" onSubmit={handleSubmit(criarSenha)}>
-                <img  src="/src/assets/images/logo.jpeg" alt="Logo"   />
+                <img  src="/src/assets/images/logo_mc.jpeg" alt="Logo"   />
+                <h2>Esqueceu sua senha?</h2>
 
                 <h5>Digite o E-mail cadastrado para redefinição de senha:</h5>
 
                     <Form.Group className="col-8" controlid="Email">
                         <Form.Label>E-mail:</Form.Label>
-                        {/* <Form.Control type="email" placeholder="Digite seu email" {...register("Email", { required: true })} /> */}
                         <Form.Control type="email" placeholder="Digite seu email" {...register("email", { required: true })} />
 
                         {errors.email && <span>Campo Obrigatório</span>}
