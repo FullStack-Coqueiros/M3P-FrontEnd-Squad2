f69ee13e19ddc81237dbd741fd09b52668de072a
josicaroline <joseanecaroline@hotmail.com>
2023-10-29 17:58:08 -0300

    bugfix login

--- project_frontend/src/services/Login.js
+++ project_frontend/src/services/Login.js
@@ -1,11 +1,11 @@
 import { URL_API } from "."
 
-export async function CheckLogin(Email, Senha) {
+export async function CheckLogin(tentativaLogin) {
     const resposta = await fetch(`${URL_API}/usuarios`)
     const usuarios = await resposta.json()
     const usuario = usuarios.find(usuario => 
-                                    usuario.Email === Email 
-                                    && usuario.Senha === Senha)
+                                    usuario.Email === tentativaLogin.Email 
+                                    && usuario.Senha === tentativaLogin.Senha)
     
     return usuario
 }
